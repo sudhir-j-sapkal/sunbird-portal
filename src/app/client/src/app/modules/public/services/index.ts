2af9eb775772b58477b563b9c6588d0f143d4fe5
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2018-08-01 10:50:12 +0530

    Issue #SB-5684 task: bring logo on app notification

--- src/app/client/src/app/modules/public/services/index.ts
+++ src/app/client/src/app/modules/public/services/index.ts
@@ -2,3 +2,4 @@ export * from './signup.service';
 export * from './public-player/public-player.service';
 export * from  './signup-guard/signup.guard';
 export * from './landingpage-guard/landingpage.guard';
+export * from '../../core/services/tenant/tenant.service';
